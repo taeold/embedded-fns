e7dc8aae5926596b9c655f8ead86338903a9dfa7
Daniel Young Lee <danielylee@google.com>
2024-06-11 11:22:37 -0700

    Break things more subtely

--- server.ts
+++ server.ts
@@ -5,6 +5,8 @@ import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
 
+aslkdjasldkj
+
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
